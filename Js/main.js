9edf71d020c58c538227f81e1e572983e6f1a4d2
MacarenaCerutti <42657225+MacarenaCerutti@users.noreply.github.com>
2022-01-28 19:13:15 -0300

    Actualización

--- Js/main.js
+++
@@ -1,7 +0,0 @@
-function monedaConver() {
-    let dolar = 102;
-    let ars = parseInt(document.getElementById("ars").value);
-    let resultado = ars / dolar;
-
-    document.getElementById("resultado").innerHTML = resultado;
-}
