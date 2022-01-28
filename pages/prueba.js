11c91094ffb2f95e8b21584b25f951f1bb5d4c6d
alejandroibz <coder.alejandro@gmail.com>
2022-01-28 15:52:46 -0300

    add prueba

--- pages/prueba.js
+++
@@ -1,7 +0,0 @@
-function monedaConver() {
-    let dolar = 102.60;
-    let ars = parseInt(document.getElementById("ars").value);
-    let resultado = ars / dolar;
-    
-    document.getElementById("resultado").innerHTML = resultado;
-}
