5f8352e92a0d230a5357541963e57e63c279c8b2
alejandroibz <coder.alejandro@gmail.com>
2022-02-01 19:33:37 -0300

    Log in pop up

--- Js/js.js
+++ Js/js.js
@@ -5,3 +5,27 @@ function monedaConver() {
     
     document.getElementById("resultado").innerHTML = resultado;
 }
+
+// ============= JS DEL LOGIN ==============
+
+// Declaramos distintas variables para llamar a los elementos contenedores
+var btnIniciaSesion = document.getElementById('btnIniciaSesion'),
+    div = document.getElementById('divInicioSesion'),
+    popup = document.getElementById('divInicioSesion1'),
+    btnCerrarVentana = document.getElementById('btnCerrarVentana');
+
+
+// Generamos una funcion en la que al clickear el boton le agregue una clase a los div correspondientes asi pudiendose mostrar
+btnIniciaSesion.addEventListener('click', function () {
+    div.classList.add('active');
+    divInicioSesion1.classList.add('active');
+});
+
+
+// Misma logica pero al reves, se le remueve la clase para cerrar la ventana
+btnCerrarVentana.addEventListener('click', function (e) {
+    e.preventDefault();
+    div.classList.remove('active');
+    divInicioSesion1.classList.remove('active');
+});
+
