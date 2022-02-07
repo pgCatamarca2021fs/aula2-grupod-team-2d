ea2277c2071903907f30368d4eda5db216c8a0d1
claudioNievaG <claudionieva.g@gmail.com>
2022-02-07 02:35:40 -0300

    migrado a Angular parte 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainIndexComponent } from './main-index/main-index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainIndexComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main-index/main-index.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-index',
+  templateUrl: './main-index.component.html',
+  styleUrls: ['./main-index.component.css']
+})
+export class MainIndexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
