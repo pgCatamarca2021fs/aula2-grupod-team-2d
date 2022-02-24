a022445e1238007e607b9c3e3006eb0d2a268c0e
MacarenaCerutti <42657225+MacarenaCerutti@users.noreply.github.com>
2022-02-24 18:48:48 -0300

    validacionesRegistro

---
+++ src/app/utils/mis-validaciones.ts
@@ -0,0 +1,33 @@
+//import {AbstractControl} from '@angular/forms';
+
+//export class MisValidaciones{
+  //  static valTexto(control: AbstractControl ){ //hasta acá sería todo igual (?)
+      //  const value = control.value;
+    //    if(value.minLength>=5){
+        //    alert("rrjnr");
+        //}
+ //   }
+//}
+
+import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
+
+export function createPasswordStrengthValidator(): ValidatorFn {
+    return (control:AbstractControl) : ValidationErrors | null => {
+
+        const value = control.value;
+
+        if (!value) {
+            return null;
+        }
+
+        const hasUpperCase = /[A-Z]+/.test(value);
+
+        const hasLowerCase = /[a-z]+/.test(value);
+
+        const hasNumeric = /[0-9]+/.test(value);
+
+        const passwordValid = hasUpperCase && hasLowerCase && hasNumeric;
+
+        return !passwordValid ? {passwordStrength:true}: null;
+    }
+}
