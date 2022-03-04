f27089d67576251e06fc743355eb89cde131e11f
alejandroibz <coder.alejandro@gmail.com>
2022-03-04 20:42:33 -0300

    css sobre nosotros

--- src/app/navbar-lateral/navbar-lateral.component.ts
+++ src/app/navbar-lateral/navbar-lateral.component.ts
@@ -7,7 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarLateralComponent implements OnInit {
 
-  constructor() { }
+  show = true;
+  body = "";
+  menu__side = "";
+  estado1 = true;
+  estado2 = false;
+  clases = {
+    'body_move menu__side_move ': true
+  }
+
+  openMenu() {
+    this.body = "body_move";
+    this.menu__side = "menu__side_move";
+  }
+
+  closeMenu() {
+    this.body = "";
+    this.menu__side = "";
+  }
 
   ngOnInit(): void {
   }
