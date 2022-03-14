2e2819edce2bb60073e8415342fb87db160f9901
Alejandro Ibañez <coder.alejandro@gmail.com>
2022-03-14 14:44:23 -0300

    backend

---
+++ CataEchange/CataEchange/Models/GestorComercia.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CataEchange.Models
+{
+    public class GestorComercia
+    {
+        string conectionString = ConfigurationManager.ConnectionStrings["CataExchange"].ToString();
+
+        public List<Comercia> ListaComercia()
+        {
+            List<Comercia> listadoComerce = new List<Comercia>();
+
+            using (SqlConnection connection = new SqlConnection(this.conectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = "listaComercia";
+                command.CommandType = CommandType.StoredProcedure;
+                SqlDataReader dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    Comercia comerce = new Comercia();
+                    comerce.IdComercia = dataReader.GetInt32(0);
+                    comerce.IdCuentaPesos = dataReader.GetInt32(1);
+                    comerce.IdCriptoMoneda = dataReader.GetInt32(2);
+                    comerce.IdBilletera = dataReader.GetInt32(3);
+                    comerce.CantidadCripto = dataReader.GetFloat(4);
+                    comerce.CantidadPesos = dataReader.GetFloat(5);
+                    comerce.TipoOperacion = dataReader.GetString(6);
+                    comerce.FechaHora = dataReader.GetDateTime(7);
+
+                    listadoComerce.Add(comerce);
+                }
+            }
+            return listadoComerce;
+        }
+
+        public void ModificarComercia(Comercia Comercia)
+        {
+            using (SqlConnection connection = new SqlConnection(this.conectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = "modificarGestorComercia";
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.Add(new SqlParameter("@idCuentaPesos",Comercia.IdCuentaPesos));
+                command.Parameters.Add(new SqlParameter("@idCriptoMoneda", Comercia.IdCriptoMoneda));
+                command.Parameters.Add(new SqlParameter("@idBilletera", Comercia.IdBilletera));
+                command.Parameters.Add(new SqlParameter("@cantidadCripto", Comercia.CantidadCripto));
+                command.Parameters.Add(new SqlParameter("@cantidadPesos", Comercia.CantidadPesos));
+                command.Parameters.Add(new SqlParameter("@tipoDeOperacion", Comercia.TipoOperacion));
+                command.Parameters.Add(new SqlParameter("@fechaHora", Comercia.FechaHora));
+
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+
+    }
+}
---
+++ CataEchange/CataEchange/Models/GestorEnviaDeposita.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CataEchange.Models
+{
+    public class GestorEnviaDeposita
+    {
+        string conectionString = ConfigurationManager.ConnectionStrings["CataExchange"].ToString();
+
+        public List<EnviaDeposita> ListaEnviaDeposita()
+        {
+            List<EnviaDeposita> listaEnDe = new List<EnviaDeposita>();
+
+            using (SqlConnection connection = new SqlConnection(this.conectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = "listaEnviaDeposita";
+                command.CommandType = CommandType.StoredProcedure;
+                SqlDataReader dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    EnviaDeposita enDe = new EnviaDeposita();
+                    enDe.IdEnviaDeposita = dataReader.GetInt32(0);
+                    enDe.IdUsuario = dataReader.GetInt32(1);
+                    enDe.IdCuentaPesos = dataReader.GetInt32(2);
+                    enDe.Monto = dataReader.GetFloat(3);
+                    enDe.TipoOperacion = dataReader.GetString(4);
+
+                    listaEnDe.Add(enDe);
+                }
+            }
+            return listaEnDe;
+        }
+
+
+        public void ModificarEnviaDeposita(EnviaDeposita enviaDeposita)
+        {
+            using (SqlConnection connection = new SqlConnection(this.conectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = "modificarEnviaDeposita";
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.Add(new SqlParameter("@idUsuario", enviaDeposita.IdUsuario));
+                command.Parameters.Add(new SqlParameter("@idCuentaPesos", enviaDeposita.IdCuentaPesos));
+                command.Parameters.Add(new SqlParameter("@monto", enviaDeposita.Monto));
+                command.Parameters.Add(new SqlParameter("@tipoOperacion", enviaDeposita.TipoOperacion));
+
+
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
