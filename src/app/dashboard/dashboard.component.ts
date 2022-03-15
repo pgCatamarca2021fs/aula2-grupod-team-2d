25f4f876e61cc6a1c842d10989b82251a842b76d
Claudio <90139265+claudioNievaG@users.noreply.github.com>
2022-03-15 18:30:19 -0300

    Revert "api service coingecko agregada"
    This reverts commit f34dd1b447baf72551752918e34e8cee6c470d6d.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CoingeckoApiService } from '../servicios/coingecko-api.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,11 +6,6 @@ import { CoingeckoApiService } from '../servicios/coingecko-api.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-    criptomonedas: any ={};
-
-    constructor(private service: CoingeckoApiService){
-
-    }
 
   precioPeso = 0;
   precioDolar = 0;
@@ -21,11 +15,9 @@ export class DashboardComponent implements OnInit {
 
 
 
-  // constructor() { }
+  constructor() { }
 
   ngOnInit(): void {
-      this.service.getAllCoins().subscribe(criptomonedas => this.criptomonedas = criptomonedas)
   }
 
 }
-
