dec541d5a5653b4a5d84867a8424bd812c061708
Claudio <90139265+claudioNievaG@users.noreply.github.com>
2022-03-16 15:59:37 -0300

    ajustando price-average en div-sup-der

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,11 +8,17 @@ import { CoingeckoApiService } from '../servicios/coingecko-api.service';
 })
 export class DashboardComponent implements OnInit {
     criptomonedas: any =[];
+    precioBitcoin: string = "";
+
+    rounded = (number:number) => {
+        return Math.round(number);
+    }
 
     constructor(private service: CoingeckoApiService){
 
     }
-
+    
+// caluculadora dolar blue
   precioPeso = 0;
   precioDolar = 0;
   monedaConver(precioPeso: number, precioDolar: number) {
@@ -22,10 +28,19 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
       this.service.getAllCoins().subscribe(criptomonedas => {
           this.criptomonedas = criptomonedas;
+
+            // INDICE PARA PROBAR CADA ARREGLO
           console.log(this.criptomonedas);
+
+          // GUARDO VALORES NECESARIOS PARA SU POSTERIOR USO EJEM: PRECIO BITCOIN
+          this.precioBitcoin = criptomonedas[0].current_price;
+          // console.log(this.precioBitcoin)
           console.log(criptomonedas[2].symbol)
+          
+          console.log(criptomonedas[0].price_change_percentage_24h);
       })
+
+      
   }
 
 }
-
