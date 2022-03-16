7d3fd0b6b8430dfd566b2bd15df4994606799868
Claudio <90139265+claudioNievaG@users.noreply.github.com>
2022-03-16 15:57:59 -0300

    agregando api y consumiendo servicios coingecko en swap

--- src/app/swap/swap.component.ts
+++ src/app/swap/swap.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CoingeckoApiService } from '../servicios/coingecko-api.service';
 
 @Component({
   selector: 'app-swap',
@@ -6,15 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./swap.component.css']
 })
 export class SwapComponent implements OnInit {
+
+  criptomonedas: any =[];
+
+
+  formatoPrecio = (price: number) => {
+    let opDivisa = { style: 'currency', currency: 'ars' };
+    let formatNum = new Intl.NumberFormat('us-US', opDivisa);
+    return formatNum.format(price)
+  }
+ 
+
+
   
   precioPeso = 0;
   precioDolar = 0;
   monedaConver(precioPeso: number, precioDolar:number) {
     precioDolar = precioPeso * 210;
 }
-  constructor() { }
 
-  ngOnInit(): void {
+  
+
+  constructor(private service: CoingeckoApiService){
+
+    }
+
+    ngOnInit(): void {
+      this.service.getAllCoins().subscribe(criptomonedas => {
+          this.criptomonedas = criptomonedas;
+
+          this.criptomonedas;
+            // INDICE PARA PROBAR CADA ARREGLO
+          console.log(this.criptomonedas);
+
+          // GUARDO VALORES NECESARIOS PARA SU POSTERIOR USO EJEM: PRECIO BITCOIN
+          // this.precioBitcoin = criptomonedas[0].current_price;
+          // console.log(this.precioBitcoin)
+          console.log(criptomonedas[2].symbol)
+          
+      })
+
+      
   }
-
 }
