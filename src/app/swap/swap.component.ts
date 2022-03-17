4b3276428ca41bd75ac10bc3a7509e27594ea5ad
Claudio <90139265+claudioNievaG@users.noreply.github.com>
2022-03-16 22:17:19 -0300

    solucionando precio Pesos swap prueba

--- src/app/swap/swap.component.ts
+++ src/app/swap/swap.component.ts
@@ -12,7 +12,7 @@ export class SwapComponent implements OnInit {
 
 
   formatoPrecio = (price: number) => {
-    let opDivisa = { style: 'currency', currency: 'USD' };
+    let opDivisa = { style: 'currency', currency: 'ARS' };
     let formatNum = new Intl.NumberFormat('us-US', opDivisa);
     return formatNum.format(price)
   }
