aa9db6613d7bf2ca186d8d93309a5cd49ff225fd
Claudio <90139265+claudioNievaG@users.noreply.github.com>
2022-03-23 18:00:52 -0300

    enviando criptomonedas valor 0 a base de datos

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {BilleterasService} from '../servicios/billeteras.service';
 import { CoingeckoApiService } from '../servicios/coingecko-api.service';
 import {UsuarioService} from '../servicios/usuario.service';
 
@@ -10,6 +11,81 @@ import {UsuarioService} from '../servicios/usuario.service';
 export class DashboardComponent implements OnInit {
     criptomonedas: any =[];
     precioBitcoin: string = "";
+    billeteraUsuario: any = [];
+
+    crearCriptos: any = [
+        
+        {
+          idUsuario: Number(localStorage.getItem('idUsuario')),
+          clavePublica: "87ao6e5u876oa45ua4oe65",
+          nombreCripto: "Bitcoin",
+          cantidadCripto: 0,
+          cotizacion: 0
+        },        
+        {
+          idUsuario: Number(localStorage.getItem('idUsuario')),
+          clavePublica: "87ao6e5u876oa45ua4oe65",
+          nombreCripto: "Ethereum",
+          cantidadCripto: 0,
+          cotizacion: 0
+        },        
+        {
+          idUsuario: Number(localStorage.getItem('idUsuario')),
+          clavePublica: "87ao6e5u876oa45ua4oe65",
+          nombreCripto: "Tether",
+          cantidadCripto: 0,
+          cotizacion: 0
+        },        
+        {
+          idUsuario: Number(localStorage.getItem('idUsuario')),
+          clavePublica: "87ao6e5u876oa45ua4oe65",
+          nombreCripto: "Binancecoin",
+          cantidadCripto: 0,
+          cotizacion: 0
+        },       
+        {
+          idUsuario: Number(localStorage.getItem('idUsuario')),
+          clavePublica: "87ao6e5u876oa45ua4oe65",
+          nombreCripto: "Cardano",
+          cantidadCripto: 0,
+          cotizacion: 0
+        },       
+        {
+          idUsuario: Number(localStorage.getItem('idUsuario')),
+          clavePublica: "87ao6e5u876oa45ua4oe65",
+          nombreCripto: "Solana",
+          cantidadCripto: 0,
+          cotizacion: 0
+        },       
+        {
+          idUsuario: Number(localStorage.getItem('idUsuario')),
+          clavePublica: "87ao6e5u876oa45ua4oe65",
+          nombreCripto: "Polkadot",
+          cantidadCripto: 0,
+          cotizacion: 0
+        },        
+        {
+          idUsuario: Number(localStorage.getItem('idUsuario')),
+          clavePublica: "87ao6e5u876oa45ua4oe65",
+          nombreCripto: "Dogecoin",
+          cantidadCripto: 0,
+          cotizacion: 0
+        },        
+        {
+          idUsuario: Number(localStorage.getItem('idUsuario')),
+          clavePublica: "87ao6e5u876oa45ua4oe65",
+          nombreCripto: "Dai",
+          cantidadCripto: 0,
+          cotizacion: 0
+        },       
+        {
+          idUsuario: Number(localStorage.getItem('idUsuario')),
+          clavePublica: "87ao6e5u876oa45ua4oe65",
+          nombreCripto: "Smooth-love-potion",
+          cantidadCripto: 0,
+          cotizacion: 0
+        },
+    ]
 
     rounded = (number:number) => {
         return Math.round(number);
@@ -17,7 +93,8 @@ export class DashboardComponent implements OnInit {
 
     constructor(
         private service: CoingeckoApiService,
-        private usuarioService: UsuarioService
+        private usuarioService: UsuarioService,
+        private billeteraService: BilleterasService
     ){
 
     }
@@ -44,20 +121,34 @@ export class DashboardComponent implements OnInit {
           console.log(criptomonedas[0].price_change_percentage_24h);
       })
 
+// LISTAR USUARIO PARA EXTRAER EL ID
       this.usuarioService.listarUsuario().subscribe(
           usuarios => {
               for (let usuario of usuarios){
                   if (usuario.Email == localStorage.getItem('Email')){
                     localStorage.setItem('idUsuario', usuario.idUsuario)
-                    console.log(usuario);
                   }
               }
+
               // this.usuarios = usuarios;
           }
       );
 
-
       
+// AGREGANDO CRIPTOMONEDAS A USUARIOS
+    /*   if(this.billeteraUsuario){ */
+/*  */
+      /* } */
+   /*  for(let criptoACrear of this.crearCriptos ){ */
+        /* console.log(criptoACrear) */
+        /* this.billeteraService.crearBilletera(criptoACrear).subscribe(); */
+    /* } */
+
+    this.billeteraService.listarBilletera().subscribe(
+                billeteras => {
+                    console.log(billeteras)
+                }
+            )
   }
 
 }
